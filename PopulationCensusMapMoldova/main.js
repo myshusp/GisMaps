01d30552f58f9736de8179e3c882439b83c3c5b8
Mihai Spataru <myshusp@gmail.com>
2023-12-05 14:48:28 +0200

    Adauga actualizari.

--- PopulationCensusMapMoldova/main.js
+++ PopulationCensusMapMoldova/main.js
@@ -804,7 +804,7 @@ mapboxgl.accessToken = 'pk.eyJ1IjoibXlzaHVzcCIsImEiOiJja2lyZmtvNHgyNTZtMnFxanR1c
 
         document.getElementById('pd').innerHTML = states.length
             ?`  
-                <canvas id="myChart" style="width:100%;max-width:600px"></canvas>
+                <canvas id="myChart"></canvas>
             ` //.toLocaleString()
             : `<p>Alegeți o unitate administrativă...</p>`;
 
