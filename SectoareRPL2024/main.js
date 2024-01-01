69da8494f889d8ee2c6f36b26a72aa2880e9aa90
Mihai Spataru <myshusp@gmail.com>
2024-01-01 22:41:27 +0200

    Adauga actualizari harta.

--- SectoareRPL2024/main.js
+++ SectoareRPL2024/main.js
@@ -2,94 +2,155 @@ mapboxgl.accessToken = 'pk.eyJ1IjoibXlzaHVzcCIsImEiOiJja2lyZmtvNHgyNTZtMnFxanR1c
     const map = new mapboxgl.Map({
         container: 'map',
         // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-        style: 'mapbox://styles/myshusp/clomgkp5i009w01prgx844h9h',
+        style: 'mapbox://styles/myshusp/clqvajjpr00yp01o91lrz3zg0',
         center: [28.167, 47.01],
         zoom: 6.8,
         minZoom: 6,
         
     });
 
-    // Add layer source
-    map.on('load', () => {
-        // make a pointer cursor
-        map.getCanvas().style.cursor = 'default';
-        // define layers sources and names
+
+    // Add GeoJSON source and layer
+    function addAdditionalSourceAndLayer() {
+
         map.addSource('lau', {
             'type': 'geojson',
-            'data': 'https://myshusp.github.io/GisMaps/SectorizareRPL2024/geojson/Sectorizare_raioane_VD.geojson'
+            'data': 'https://myshusp.github.io/GisMaps/SectoareRPL2024/geojson/Sectorizare_raioane_VD.geojson'
         });
-        //Add layer
         map.addLayer(
             {
-                'id': 'lau2_sectoare',
-                'source': 'lau',
-                'type': 'fill',
-                'paint': {
-                    'fill-color': [
-                        'interpolate',
-                        ['linear'],
-                        ['get', 'VD_crs'],
-                        0,
-                        'grey',
-                        1,
-                        'red',
-                        2,
-                        'yellow',
-                        3,
-                        'blue',
-                        4,
-                        'green'
-                    ],
-                    'fill-opacity': 0.6,
-                    'fill-outline-color':'#ffffff'
-                },
-                'filter': ['==', 'UNITLVLID', 5]
-            },
-        );
-
-        map.addLayer(
-            {
-                'id': 'lau1-line-border',
+                'id': 'district-line-border',
                 'source': 'lau',
                 'type': 'line',
                 'layout': {
                     'visibility': 'visible',
                 },
                 'paint': {
-                    'line-width': 1,
+                    'line-width': 4,
                     'line-opacity': 1,
-                    'line-color':'black'
+                    'line-color':'red'
                 },
                 'filter': ['==', 'UNITLVLID', 4]
             },
         );
 
-        map.addLayer({
-                'id': 'communes-labels',
-                'source': 'lau',
-                'minzoom': 9,
-                'type': 'symbol',
-                'symbol-avoid-edges': true,
-                'text-allow-overlap': true,
-                'symbol-placement': 'point',
-                'font-scale': 100,
+        map.addSource('sectoare-supv', {
+            'type': 'geojson',
+            'data': 'https://myshusp.github.io/GisMaps/SectoareRPL2024/geojson/Sectoare_supv.geojson'
+        });
+        map.addLayer(
+            {
+                'id': 'sectoare-supv-polygons',
+                'source': 'sectoare-supv',
+                'type': 'fill',
                 'layout': {
-                    'visibility': 'visible',
-                    'text-field': ['get', 'labels'],
-                    'text-variable-anchor': ['center'],
-                    'text-radial-offset': 1.5,
-                    'text-justify': 'center',
-                    'text-size': 12,
-                    'text-padding': 2,
-                    'text-font': ['Open Sans Regular'],
-                    // 'text-offset': [0.85, -1.7],
+                    'visibility': 'none',
                 },
                 'paint': {
-                "text-color": "white",
+                    'fill-color': '#00ffff',
+                    'fill-opacity': 0.6,
+                    // 'fill-outline-color':'#ffffff'
                 },
-                'filter': ['==', 'UNITLVLID', 5]
+            },
+            'district-line-border'
+        );
+        map.addLayer(
+            {
+                'id': 'sectoare-supv-line-border',
+                'source': 'sectoare-supv',
+                'type': 'line',
+                'layout': {
+                    'visibility': 'visible',
+                },
+                'paint': {
+                    'line-width': 2,
+                    'line-opacity': 1,
+                    'line-color':'blue'
+                },
+            },
+            'district-line-border'
+        );
+
+        map.addSource('sectoare-enum', {
+            'type': 'geojson',
+            'data': 'https://myshusp.github.io/GisMaps/SectoareRPL2024/geojson/Sectoare_enum.geojson'
+        });
+        map.addLayer(
+            {
+                'id': 'sectoare-enum-polygons',
+                'source': 'sectoare-enum',
+                'type': 'fill',
+                'layout': {
+                    'visibility': 'none',
+                },
+                'paint': {
+                    'fill-color': '#00ffff',
+                    'fill-opacity': 0.6,
+                    // 'fill-outline-color':'#ffffff'
+                },
+            },
+            'sectoare-supv-line-border'
+        );
+        map.addLayer(
+            {
+                'id': 'sectoare-enum-line-border',
+                'source': 'sectoare-enum',
+                'type': 'line',
+                'layout': {
+                    'visibility': 'visible',
+                },
+                'paint': {
+                    'line-width': 1.5,
+                    'line-opacity': 1,
+                    'line-color':'black'
+                },
+            },
+            'sectoare-supv-line-border'
+        );
+        
+
+        map.addSource('sectoare', {
+            'type': 'geojson',
+            'data': 'https://myshusp.github.io/GisMaps/SectoareRPL2024/geojson/Sectoare.geojson'
         });
 
+        map.addLayer(
+            {
+                'id': 'sectoare-polygons',
+                'source': 'sectoare',
+                'type': 'fill',
+                'layout': {
+                    'visibility': 'visible',
+                },
+                'paint': {
+                    'fill-color': 'magenta',
+                    'fill-opacity': 0.3,
+                    // 'fill-outline-color':'white',
+                    
+                },
+            },
+            'sectoare-enum-line-border'
+        );
+        map.addLayer(
+            {
+                'id': 'sectoare-line-border',
+                'source': 'sectoare',
+                'type': 'line',
+                'layout': {
+                    'visibility': 'visible',
+                },
+                'paint': {
+                    'line-width': 0.5,
+                    'line-opacity': 1,
+                    'line-color':'white'
+                },
+            },
+            'sectoare-enum-line-border'
+        );
+    }
+
+    map.on('style.load', () => {
+        addAdditionalSourceAndLayer();
     });
 
     // Add a scale control to the map
@@ -98,3 +159,22 @@ mapboxgl.accessToken = 'pk.eyJ1IjoibXlzaHVzcCIsImEiOiJja2lyZmtvNHgyNTZtMnFxanR1c
     map.addControl(new mapboxgl.FullscreenControl());
     // Add zoom and rotation controls to the map.
     map.addControl(new mapboxgl.NavigationControl());
+
+        //Add collapsible Caracteristici (map-overlay)
+        $(function () {
+            $(".button-open-map-overlay").hide();
+            $(".button-close-map-overlay").bind("click", function () {
+                $(".map-overlay").hide(100);        
+    
+                if ($(this).attr("class") == "button-close-map-overlay") {
+                    $(".button-open-map-overlay").show();
+                }
+            });
+            $(".button-open-map-overlay").bind("click", function () {
+                $(".map-overlay").show(100);        
+                if ($(this).attr("class") == "button-open-map-overlay")
+                {
+                    $(".button-open-map-overlay").hide();
+                }
+                });
+        }); 
